acef24683bd45706181b894d4d0191ced5db5a1a
Daniil Goncharov <twnz778@gmail.com>
2020-10-13 16:26:57 +0300

    comments off

--- src/api-service/Reddit-api.js
+++ src/api-service/Reddit-api.js
@@ -1,9 +1,9 @@
 const fetchPost = async (name) => {
     const res = await fetch(`https://www.reddit.com/r/${name}.json`);
   
-    // if (!res.ok) {
-    //   throw new Error(`Could not fetch!!! recived status: ${res.status}`);
-    // }
+    if (!res.ok) {
+      throw new Error(`Could not fetch!!! recived status: ${res.status}`);
+    }
   
     return res.json();
   };
