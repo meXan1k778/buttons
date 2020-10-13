462bd0009b280af48e198b0253a386d6cf3c7c15
Daniil Goncharov <twnz778@gmail.com>
2020-10-13 16:25:34 +0300

    comments off

--- src/reducers/rootReducer/rootReducer.js
+++ src/reducers/rootReducer/rootReducer.js
@@ -20,24 +20,10 @@ const Reducer = (state = initilaState, action) => {
         return {...state, posts: newArray}
     }
     if(action.type === "SORT"){
-    //    const source = action.payload.sourceIdx
-    //    const dest = action.payload.destIdx
-    //     const oldArray = state.posts.slice()
-    //     oldArray[source] = state.posts[dest]
-    //     oldArray[dest] = state.posts[source]
-    //     console.log(oldArray)
+   
     const arr = state.posts.filter((item,index)=> index !== action.payload.sourceIdx)
     arr.splice(action.payload.destIdx, 0, state.posts[action.payload.sourceIdx])
-        // const newArray = state.posts.map((item, index)=> {
-        //     if(index === action.payload.sourceIdx){
-        //         return state.posts[action.payload.destIdx]
-        //     }
-        //     if(index === action.payload.destIdx){
-        //         return state.posts[action.payload.sourceIdx]
-        //     }
-        //     return item
-        // }  )
-        
+    
         return {...state, posts: arr}
     }
     return state
